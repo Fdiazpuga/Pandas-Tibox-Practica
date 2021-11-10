edfc6bf477b97a287683ad073b51f06d16f5b139
Fabian Díaz <fdiaz@tibox.cl>
2021-11-10 17:04:53 -0300

    sadasd

---
+++ untitled0.py
@@ -0,0 +1,27 @@
+from openpyxl import load_workbook
+
+import pandas as pd
+
+
+
+wb = load_workbook(filename='2022 Carga MBI v3.xlsx', data_only=True)
+
+
+
+ws = wb['MBI META']
+
+
+
+data = ws.values
+
+# Get the first line in file as a header line
+
+columns = next(data)[0:]
+
+# Create a DataFrame based on the second and subsequent lines of data
+
+df = pd.DataFrame(data, columns=columns)
+
+
+
+df.head(30)
